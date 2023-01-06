3ec4f1985a47a099a128c904e59e515ef7dcdb15
Sam Warring <samwarring@gmail.com>
2023-01-06 14:07:43 -0800

    Add UI for setting group colors

--- popup.js
+++ popup.js
@@ -2,8 +2,19 @@
 let options = {
     enableSort: true,
     enableGroups: true,
-    groupThreshold: 4
+    groupThreshold: 4,
+    groupColors: new Map([
+        ["google", "blue"],
+        ["stackoverflow", "orange"],
+        ["duckduckgo", "red"],
+    ])
 };
+
+const defaultGroupColors = new Map([
+    ["google", "blue"],
+    ["stackoverflow", "orange"],
+    ["duckduckgo", "red"],
+]);
 
 const controller = {
     enableSort: async function(isEnabled) {
@@ -30,29 +41,89 @@ const controller = {
         }
     },
 
+    addGroupColor: async function(groupName, color) {
+        if (groupName) {
+            options.groupColors.set(groupName, color);
+            // TODO: Persist options
+        }
+    },
+
+    removeGroupColor: async function(groupName) {
+        if (groupName) {
+            options.groupColors.delete(groupName);
+            // TODO: Persist options
+        }
+    },
+
     loadOptions: async function() {
+        //await chrome.storage.sync.clear();
         const data = await chrome.storage.sync.get("options");
         Object.assign(options, data.options);
+        if (!(options.groupColors instanceof Map)) {
+            options.groupColors = new Map();
+        }
         console.log("LOAD OPTIONS:", options);
     },
 
     storeOptions: async function() {
+        if (!(options.groupColors instanceof Map)) {
+            options.groupColors = new Map();
+        }
         console.log("STORE OPTIONS:", options);
         chrome.storage.sync.set({ options });
-    }
+    },
 };
 
 const ui = {
+    collator: new Intl.Collator(),
     enableSort: document.getElementById("enableSort"),
     enableGroups: document.getElementById("enableGroups"),
     groupThreshold: document.getElementById("groupThreshold"),
     decreaseGroupThreshold: document.getElementById("decreaseGroupThreshold"),
     increaseGroupThreshold: document.getElementById("increaseGroupThreshold"),
+    newGroupColorAdd: document.getElementById("newGroupColorAdd"),
+    newGroupColorColor: document.getElementById("newGroupColorColor"),
+    newGroupColorName: document.getElementById("newGroupColorName"),
+    groupColorList: document.getElementById("groupColorList"),
+    groupColorTemplate: document.getElementById("groupColorTemplate"),
 
     updateAll: function() {
         this.enableSort.checked = options.enableSort;
         this.enableGroups.checked = options.enableGroups;
         this.groupThreshold.textContent = String(options.groupThreshold);
+        this.updateGroupColorSelect();
+        this.updateGroupColors();
+    },
+
+    updateGroupColorSelect: function() {
+        const value = this.newGroupColorColor.value;
+        const color = "var(--color-tab-" + value + ")";
+        this.newGroupColorColor.style.backgroundColor = color;
+    },
+
+    updateGroupColors: function() {
+        this.groupColorList.textContent = '';
+        const entries = Array.from(options.groupColors.entries());
+        entries.sort((e1, e2) => this.collator.compare(e1[0], e2[0]));
+        for (const groupColor of entries) {
+            const groupName = groupColor[0];
+            const color = groupColor[1];
+            this.addGroupColor(groupName, color);
+        }
+    },
+
+    addGroupColor: function(groupName, color) {
+        const listItem = this.groupColorTemplate.content.firstElementChild.cloneNode(true);
+        const button = listItem.querySelector("input[type='button']");
+        const label = listItem.querySelector("label");
+        label.textContent = groupName;
+        label.classList.add(color);
+        this.groupColorList.append(listItem);
+
+        button.addEventListener("click", () => {
+            controller.removeGroupColor(groupName);
+            this.groupColorList.removeChild(listItem);
+        });
     },
 
     addEventListeners: function() {
@@ -69,6 +140,15 @@ const ui = {
         this.increaseGroupThreshold.addEventListener("click", async () => {
             await controller.increaseGroupThreshold();
             this.groupThreshold.textContent = String(options.groupThreshold);
+        });
+        this.newGroupColorColor.addEventListener("change", (event) => {
+            this.updateGroupColorSelect();
+        });
+        this.newGroupColorAdd.addEventListener("click", async () => {
+            const groupName = this.newGroupColorName.value;
+            const color = this.newGroupColorColor.value;
+            await controller.addGroupColor(groupName, color);
+            this.updateGroupColors();
         });
     }
 };
